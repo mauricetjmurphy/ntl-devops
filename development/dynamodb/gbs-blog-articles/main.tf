e758b3db637823f5fc8c8a5ca853db822508c433
Maurice.Murphy <maurice.murphy@nbcuni.com>
2023-04-23 12:43:31 +0100

    Test dev article db with no index

--- development/dynamodb/gbs-blog-articles/main.tf
+++ development/dynamodb/gbs-blog-articles/main.tf
@@ -12,26 +12,14 @@ resource "aws_dynamodb_table" "main" {
   name           = var.table_name
   billing_mode   = "PAY_PER_REQUEST"
   hash_key       = "Id"
-  range_key      = "CreatedAt"
 
   attribute {
-      name = "Id"
-      type = "S"
-    }
+    name = "Id"
+    type = "S"
+  }
 
-  attribute {
-      name = "CreatedAt"
-      type = "S"
-    }
-
-  global_secondary_index {
-    name = "CreatedAt-index"
-    hash_key = "CreatedAt"
-    range_key = "Id"
-    projection_type = "ALL"
-  }   
+  tags = {
+    Environment = var.environment
+    Project     = "GBS Blog Website"
+  }
 }
-
-
-
-
